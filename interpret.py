82efa096a6f6eb167801b7a61a0f4c6f4ac21973
quapik <73609809+quapik@users.noreply.github.com>
2021-03-26 20:38:21 +0100

    checknuty vstupy

--- interpret.py
+++ interpret.py
@@ -7,52 +7,77 @@ BylInput=False
 if (len(sys.argv)>3) or len(sys.argv)==1 :
     print("Špatný počet argumentů")
     exit(21)
-
-if (sys.argv[1]=="--help"):
-    print("Vypsat nápovědu")
-    exit(0)
-
-if(sys.argv[1][0:9]=="--source="):
-    sourcefile=sys.argv[1][9:len(sys.argv[1])]
-    BylSource=True
-    print(sourcefile)
-
-elif(sys.argv[1][0:8]=="--input="):
-    inputfile=sys.argv[1][8:len(sys.argv[1])]
-    BylInput=True
-    print(inputfile)
+if(len(sys.argv)==2):
+    if (sys.argv[1]=="--help"):
+        print("Vypsat nápovědu")
+        exit(0)
+    elif (sys.argv[1][0:9]=="--source="):
+        sourcefile=sys.argv[1][9:len(sys.argv[1])]
+        BylSource=True
+    elif(sys.argv[1][0:8]=="--input="):
+        inputfile=sys.argv[1][8:len(sys.argv[1])]
+        BylInput=True
+    else:
+        print("Chyba v argumetnech")
+        exit(10)
 else:
-    print("errror - spatny argument")
-    exit(1) #TODO
+    if(sys.argv[1][0:9]=="--source="):
+        sourcefile=sys.argv[1][9:len(sys.argv[1])]
+        BylSource=True
+        print(sourcefile)
 
-if(sys.argv[2][0:9]=="--source="):
-    if(BylSource is True):
-        print("Source podruhy")
-        exit(10) #chech exit
-    sourcefile=sys.argv[2][9:len(sys.argv[1])]
-    BylSource=True
-    print(sourcefile)
+    elif(sys.argv[1][0:8]=="--input="):
+        inputfile=sys.argv[1][8:len(sys.argv[1])]
+        BylInput=True
+        print(inputfile)
+    else:
+        print("errror - spatny argument")
+        exit(10) #TODO
 
-elif(sys.argv[2][0:8]=="--input="):
-    if(BylInput is True):
-        print("Input podruhy")
-        exit(10) #chech exit
-    inputfile=sys.argv[2][8:len(sys.argv[1])]
-    BylInput=True
-    print(inputfile)
-else:
-    print("errror - spatny argument")
-    exit(1) #TODO
+    if(sys.argv[2][0:9]=="--source="):
+        if(BylSource is True):
+            exit(10) 
+        sourcefile=sys.argv[2][9:len(sys.argv[2])]
+        BylSource=True
+        print(sourcefile)
 
-if BylInput is False and BylSource is False:
-    print("nebyl zadan ani input ani source")
-    exit(10)
+    elif(sys.argv[2][0:8]=="--input="):
+        if(BylInput is True):
+            exit(10) #chech exit
+        inputfile=sys.argv[2][8:len(sys.argv[2])]
+        BylInput=True
+        print(inputfile)
+    else:
+        print("errror - spatny argument") #DEBUG
+        exit(10) #TODO
 
-tree = ET.parse('sourcezadani.txt')
+    if BylInput is False and BylSource is False:
+        print("nebyl zadan ani input ani source") #DEBUG
+        exit(10)
+#------------------------------------------------------------------------------------------------------------------------------
+try:
+    if(BylSource==True):  #pokud je source file jako parametr, jinak stdin
+        tree = ET.parse(sourcefile)
+    else:
+        tree = ET.parse(sys.stdin)
+except IOError:
+    print("Nejde otevrit vstupni soubor") #DEBUG
+    exit(11)
 root = tree.getroot()
-print("root tag", root.tag,"\nroot atrib", root.attrib)
-print(root[1][0].text)
-#for child in root:
-    #print(child.attrib,child.tag) 
 
+try:
+    language = root.attrib['language'] #kontrola zda je atribut language a nasledne že je Ippcode
+except:
+    exit(32)
+
+if (language !='IPPcode21'):
+    print("Chybna hlavička\n") #DEBUG
+    exit(32)
+
+for child in root:
+    print("atribut", child.attrib,"tag",child.tag) 
+    print(child.attrib['opcode'])
+    for arg in child:
+        print(arg.attrib)
+print("OK")
 exit(0)
