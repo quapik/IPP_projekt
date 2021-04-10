99f986f35f83aa662d29e790072471b7e9baba79
quapik <73609809+quapik@users.noreply.github.com>
2021-04-10 23:21:03 +0200

    snad hotovo

--- test.php
+++ test.php
@@ -1,5 +1,236 @@
 <?php
 #Vojtěch Šíma, xsimav01, IPP FIT VUT 2021
+#Hlavní funkce pro prochzení složky
+function ProchazeniSlozky($directorypath)
+{
+    if (file_exists($directorypath))
+    {
+        $open_dir=opendir($directorypath);
+        while($filecheck = readdir($open_dir))
+        {   
+            if($filecheck == "." OR $filecheck == "..")
+            {
+                continue; //ignorovani . a .. složek
+            }
+            
+            if ($GLOBALS["BylRecursive"]==True) #pokud je argumentem zvoleno rekurzivni volani slozek a narazim na slozku misto souboru => volam funkci
+            {
+                if(is_dir($directorypath . "/" . $filecheck))
+                {
+                    ProchazeniSlozky(($directorypath . "/" . $filecheck));
+                }
+            }
+            
+            if(is_file($directorypath . "/" . $filecheck))
+            {
+                $directorypathfile=$directorypath . "/" . $filecheck;
+                $ext = pathinfo($directorypathfile, PATHINFO_EXTENSION);
+                
+                if ($ext == "src")
+                {   $GLOBALS["CelkemTestu"]++;
+                    $onlyfilename=pathinfo($directorypathfile, PATHINFO_FILENAME);
+                    CheckOrCreateFiles($onlyfilename,$directorypath);
+                    if($GLOBALS["IntOnly"]==True)
+                    {
+                    exec('python3.8 '.$GLOBALS["interpretfile"].' --source='.$directorypath.'/'.$filecheck.' --input='.$directorypath.'/'.$onlyfilename.'.in > outputinterpret', $output, $retval);
+                    
+                    
+                    $filerc = fopen($directorypath.'/'.$onlyfilename.'.rc', 'r'); #otervreni souboru s ocekavanym RC a precteni prvniho stringu (zbytek ignorujeme)
+                    $rc = fgets($filerc); 
+                    fclose($filerc);
+                    if($rc==$retval)
+                    {   exec('diff outputinterpret '.$directorypath.'/'.$onlyfilename.'.out >diffinterpret');
+                        if ($rc==0)
+                        {
+                            if(filesize("diffinterpret")==0) #pokud v diffsouboru nic neni zapsano tak jsou stejne outputy
+                            {
+                                $GLOBALS["CelkemTestuSpravne"]++;
+                                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>YES</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                            }
+                            else
+                            {
+                                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>NO</td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                            }
+                        }
+                        else
+                        {
+                            $GLOBALS["CelkemTestuSpravne"]++;
+                            echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>---</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                        }
+                        
+                        #odstraneni vytvorenych souboru
+                        exec('rm diffinterpret');
+                        exec('rm outputinterpret');
+                    }
+                    else
+                    {
+                        echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td> </td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                    }
+                    }
+                    else if($GLOBALS["ParseOnly"]==True) 
+                    {   
+                        $filerc = fopen($directorypath.'/'.$onlyfilename.'.rc', 'r'); #otervreni souboru s ocekavanym RC a precteni prvniho stringu (zbytek ignorujeme)
+                        $rc = fgets($filerc); 
+                        fclose($filerc);
+    
+                        exec('php7.4 '.$GLOBALS["parsefile"]. ' <'. $directorypath.'/'.$filecheck. ' >parseout', $output, $retval);
+                        
+                        if($rc==$retval)
+                        {
+                            if($retval==0)
+                            {
+                                exec('java -jar '.$GLOBALS["jexamxmlfile"].' parseout '.$directorypath.'/'.$onlyfilename.'.out delta.xml '. $GLOBALS["jexamcfgfile"]. ' >diff');
+                                $myFile = "diff";
+                                $lines = file($myFile);//file in to an array
+                                if ($lines[2] == "Two files are identical\n")
+                                {
+                                    $GLOBALS["CelkemTestuSpravne"]++;
+                                    echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>YES</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);                       
+                                }
+                                else
+                                {
+                                    echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>NO</td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                                }
+                                exec('rm diff');
+                                exec('rm delta.xml');
+                            }
+                            else
+                            {
+                                $GLOBALS["CelkemTestuSpravne"]++;
+                                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>---</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                            }
+                        
+                        }
+                        else
+                        {
+                            echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td> </td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                        }
+                        exec('rm parseout');
+                        
+    
+                    }
+                    else
+                    {
+                        $filerc = fopen($directorypath.'/'.$onlyfilename.'.rc', 'r'); #otervreni souboru s ocekavanym RC a precteni prvniho stringu (zbytek ignorujeme)
+                        $rc = fgets($filerc); 
+                        fclose($filerc);
+    
+                        exec('php7.4 '.$GLOBALS["parsefile"]. ' <'. $directorypath.'/'.$filecheck.' >parseout', $output, $retval);
+                        if ($retval!=0) #pokud nastala chyba v parseru
+                        {
+                            if ($retval==$rc)
+                            {
+                                $GLOBALS["CelkemTestuSpravne"]++;
+                                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>YES</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                            }
+                            else
+                            {
+                                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>NO</td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                            }
+                            exec('rm parseout');
+                        }
+                        else
+                        {
+                            exec('python3.8 '.$GLOBALS["interpretfile"].' --source=parseout --input='.$directorypath.'/'.$onlyfilename.'.in > outputinterpret', $output, $retval);
+                    
+                            if($rc==$retval)
+                            {   exec('diff outputinterpret '.$directorypath.'/'.$onlyfilename.'.out >diffinterpret');
+                                if ($rc==0)
+                                {
+                                    if(filesize("diffinterpret")==0) #pokud v diffsouboru nic neni zapsano tak jsou stejne outputy
+                                    {
+                                        $GLOBALS["CelkemTestuSpravne"]++;
+                                        echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>YES</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                                    }
+                                    else
+                                    {
+                                        echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>NO</td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                                    }
+                                }
+                                else
+                                {
+                                    $GLOBALS["CelkemTestuSpravne"]++;
+                            
+                                    echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>---</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                                }
+                                
+                            
+                                exec('rm diffinterpret');
+                            }
+                            else
+                            {
+                                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td> </td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
+                            }
+                        exec('rm parseout');
+                        exec('rm outputinterpret');
+                        }
+                    
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
+        
+    }
+
+#Funkce na základě jména zjisti zda potřebné soubory pro kontrolu existují a pokud ne, tak vytvoří dle zadání prázdné
+function CheckOrCreateFiles($onlyfilename,$directorypath)
+{
+if (!file_exists($directorypath . "/" .  $onlyfilename.".rc"))
+{
+    touch($directorypath . "/" .  $onlyfilename .".rc");
+    exec('echo "0">' . $directorypath . "/" .$onlyfilename . ".rc");
+}
+if (!(file_exists($directorypath . "/" .  $onlyfilename."out")))
+{
+    touch($directorypath . "/" .  $onlyfilename .".out");
+   
+}
+if (!(file_exists($directorypath . "/" .  $onlyfilename."in")))
+{
+    touch($directorypath . "/" .  $onlyfilename .".in");
+}
+}
+#Funkce na tisk uvodu html dokumentu, css, head pro první hlavní tabulku
+function HTMLStart()
+{
+echo("<!doctype html>".PHP_EOL);
+echo("<html lang=\"cz\">".PHP_EOL);
+echo("<head>".PHP_EOL);
+echo("<title>test.php pro IPPCode21</title>".PHP_EOL);
+echo("<style> ".PHP_EOL);
+echo("table, th, td {border: 1px solid black;}".PHP_EOL);
+echo("th, td { border-bottom: 1px solid #ddd;}".PHP_EOL);
+echo("th, td{text-align: center;}".PHP_EOL);
+echo("tr:hover {background-color: #f5f5f5;}".PHP_EOL);
+echo("div>#T1 { float:left;}".PHP_EOL);
+echo("div>#T2 { float:left;}".PHP_EOL);
+echo("</style>".PHP_EOL);
+echo("</head>".PHP_EOL);
+echo("<body>".PHP_EOL."<div>".PHP_EOL);
+echo("<table id=\"T1\">".PHP_EOL);
+echo("<tr>".PHP_EOL."<th>Jméno testu</th>".PHP_EOL."<th>Očekáváný návratový kód</th>".PHP_EOL."<th>Návratový kód</th>".PHP_EOL."<th>OUTPUTY</th>".PHP_EOL."<th>Výsledek</th>".PHP_EOL."</tr>".PHP_EOL);
+echo("<body>".PHP_EOL);
+}
+#Funkce pro koncové tagy htmml + tabulku s celkovou úspěšností
+function HTMLEnd()
+{
+echo("</table>".PHP_EOL);
+echo("<table id=\"T2\">".PHP_EOL);
+$Spatne=$GLOBALS["CelkemTestu"]-$GLOBALS["CelkemTestuSpravne"];
+$Procenta=($GLOBALS["CelkemTestuSpravne"]/$GLOBALS["CelkemTestu"])*100; #procentualni uspěšnost
+echo("<tr>".PHP_EOL."<th>Počet testů</th>".PHP_EOL."<th>Správně</th>".PHP_EOL."<th>Špatně</th>".PHP_EOL."<th>Výsledekv %</th>".PHP_EOL."</tr>".PHP_EOL);
+echo("<tr>".PHP_EOL."<td>".$GLOBALS["CelkemTestu"]."</td>".PHP_EOL."<td>".$GLOBALS["CelkemTestuSpravne"]."</td>".PHP_EOL."<td>".$Spatne."</td>".PHP_EOL."<td>".$Procenta."</td>".PHP_EOL."</tr>".PHP_EOL);
+echo("</table>".PHP_EOL."</div>".PHP_EOL);
+echo("</body>".PHP_EOL);
+echo("</html>".PHP_EOL);
+if (file_exists('parseout.log'))
+    {
+     exec('rm parseout.log');
+    }
+}
 $BylDircetory=$BylParseScript=$BylInterpretScript=$ParseOnly=$IntOnly=$Byljexamxml=$Byljexamcfg=false;
 $BylRecursive=false;
 $CelkemTestu=0;
@@ -111,128 +342,6 @@ if ($Byljexamxml==false)
 if ($Byljexamcfg==false)
 {
     $jexamcfgfile="/pub/courses/ipp/jexamxml/options";
-}
-
-function ProchazeniSlozky($directorypath)
-{
-
-$open_dir=opendir($directorypath);
-while($filecheck = readdir($open_dir))
-{   
-    if($filecheck == "." OR $filecheck == "..")
-    {
-        continue; //ignorovani . a .. složek
-    }
-    
-    if ($GLOBALS["BylRecursive"]==True) #pokud je argumentem zvoleno rekurzivni volani slozek a narazim na slozku misto souboru => volam funkci
-    {
-        if(is_dir($directorypath . "/" . $filecheck))
-        {
-            ProchazeniSlozky(($directorypath . "/" . $filecheck));
-        }
-    }
-    
-    if(is_file($directorypath . "/" . $filecheck))
-    {
-        $directorypathfile=$directorypath . "/" . $filecheck;
-        
-        $ext = pathinfo($directorypathfile, PATHINFO_EXTENSION);
-        if ($ext == "src")
-        {   $GLOBALS["CelkemTestu"]++;
-            $onlyfilename=pathinfo($directorypathfile, PATHINFO_FILENAME);
-            CheckOrCreateFiles($onlyfilename,$directorypath);
-            #exec('php7.4 '.$GLOBALS["parsefile"].' < '.$filecheck.' > outputfile', $output, $retval);
-            #echo(PHP_EOL.'java -jar '.$GLOBALS["jexamxmlfile"].' outputfile '.$onlyfilename.'.out delta.txt '.$GLOBALS["jexamcfgfile"].PHP_EOL);
-            #exec('java -jar '.$GLOBALS["jexamxmlfile"].' outputfile '.$onlyfilename.'.out delta.xml '.$GLOBALS["jexamcfgfile"]);
-            if($GLOBALS["IntOnly"]==True)
-            {
-            exec('python3.8 '.$GLOBALS["interpretfile"].' --source='.$directorypath.'/'.$filecheck.' --input='.$directorypath.'/'.$onlyfilename.'.in > outputinterpret', $output, $retval);
-            
-            
-            $filerc = fopen($directorypath.'/'.$onlyfilename.'.rc', 'r'); #otervreni souboru s ocekavanym RC a precteni prvniho stringu (zbytek ignorujeme)
-            $rc = fgets($filerc); 
-            fclose($filerc);
-            if($rc==$retval)
-            {   exec('diff outputinterpret '.$directorypath.'/'.$onlyfilename.'.out >diffinterpret');
-                if ($rc==0)
-                {
-                    if(filesize("diffinterpret")==0) #pokud v diffsouboru nic neni zapsano tak jsou stejne outputy
-                    {
-                        $GLOBALS["CelkemTestuSpravne"]++;
-                        echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>YES</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
-                    }
-                    else
-                    {
-                        echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>NO</td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
-                    }
-                }
-                else
-                {
-                    echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td>---</td>".PHP_EOL."<td style=\"color:green\">OK</td>".PHP_EOL."</tr>".PHP_EOL);
-                }
-                
-                
-                
-            }
-            else
-            {
-                echo("<tr>".PHP_EOL."<td>".$directorypath.'/'.$onlyfilename."</td>".PHP_EOL."<td>".$rc."</td>".PHP_EOL."<td>".$retval."</td>".PHP_EOL."<td> </td>".PHP_EOL."<td style=\"color:red\">NOT OK</td>".PHP_EOL."</tr>".PHP_EOL);
-            }
-            }
-            
-        }
-    }
-}
-}
-
-#Funkce na základě jména zjisti zda potřebné soubory pro kontrolu existují a pokud ne, tak vytvoří dle zadání prázdné
-function CheckOrCreateFiles($onlyfilename,$directorypath)
-{
-if (!file_exists($directorypath . "/" .  $onlyfilename.".rc"))
-{
-    touch($directorypath . "/" .  $onlyfilename .".rc");
-    exec('echo "0">' . $directorypath . "/" .$onlyfilename . ".rc");
-}
-if (!(file_exists($directorypath . "/" .  $onlyfilename."out")))
-{
-    touch($directorypath . "/" .  $onlyfilename .".out");
-   
-}
-if (!(file_exists($directorypath . "/" .  $onlyfilename."in")))
-{
-    touch($directorypath . "/" .  $onlyfilename .".in");
-}
-}
-
-function HTMLStart()
-{
-echo("<!doctype html>".PHP_EOL);
-echo("<html lang=\"cz\">".PHP_EOL);
-echo("<head>".PHP_EOL);
-echo("<title>test.php pro IPPCode21</title>".PHP_EOL);
-echo("<style> ".PHP_EOL);
-echo("table, th, td {border: 1px solid black;}".PHP_EOL);
-echo("th, td { border-bottom: 1px solid #ddd;}".PHP_EOL);
-echo("th, td{text-align: center;}".PHP_EOL);
-echo("tr:hover {background-color: #f5f5f5;}".PHP_EOL);
-echo("#T1 { width: 100%; background-color: #ff3399;}".PHP_EOL);
-echo("#T2 { width: 100%; background-color: #ff3399;}".PHP_EOL);
-echo("</style>".PHP_EOL);
-echo("</head>".PHP_EOL);
-echo("<body>".PHP_EOL);
-echo("<table id=\"T1>\">".PHP_EOL);
-echo("<tr>".PHP_EOL."<th>Jméno testu</th>".PHP_EOL."<th>Očekáváný návratový kód</th>".PHP_EOL."<th>Návratový kód</th>".PHP_EOL."<th>OUTPUTY</th>".PHP_EOL."<th>Výsledek</th>".PHP_EOL."</tr>".PHP_EOL);
-echo("<body>".PHP_EOL);
-}
-function HTMLEnd()
-{
-echo("<table id=\"T2>\">".PHP_EOL);
-$Spatne=$GLOBALS["CelkemTestu"]-$GLOBALS["CelkemTestuSpravne"];
-$Procenta=($GLOBALS["CelkemTestuSpravne"]/$GLOBALS["CelkemTestu"])*100;
-echo("<tr>".PHP_EOL."<th>Počet testů</th>".PHP_EOL."<th>Správně</th>".PHP_EOL."<th>Špatně</th>".PHP_EOL."<th>Výsledekv %</th>".PHP_EOL."</tr>".PHP_EOL);
-echo("<tr>".PHP_EOL."<td>".$GLOBALS["CelkemTestu"]."</td>".PHP_EOL."<td>".$GLOBALS["CelkemTestuSpravne"]."</td>".PHP_EOL."<td>".$Spatne."</td>".PHP_EOL."<td>".$Procenta."</td>".PHP_EOL."</tr>".PHP_EOL);
-echo("</body>".PHP_EOL);
-echo("</html>".PHP_EOL);
 }
 
 HTMLStart();
